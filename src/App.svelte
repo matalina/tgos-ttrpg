e5f4fb67a9be8187189aa9635c5a546884cbc2c2
Alicia Wilkerson <matalina@gmail.com>
2022-11-21 07:59:12 -0600

    added icon options

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,30 @@
 <script lang="ts">
 import DiceBox from './components/DiceBox.svelte';
 let diceBoxOpen = false;
+let page = 'home';
 
 function toggleDice() {
   diceBoxOpen = !diceBoxOpen;
 }
 
+function setPage(name: string) {
+  page = name;
+}
+
 </script>
 
-<header class="absolute top-0 left-0 w-full bg-slate-200 z-50">
+<header class="absolute top-0 left-0 w-full bg-teal-50 z-50">
   <nav>
     <button on:click={toggleDice}><i class="fa-thin fa-dice-d20"></i></button>
+    <button><i class="fa-sharp fa-solid fa-user-plus"></i></button>
+    <button><i class="fa-regular fa-address-card"></i></button>
+    <button><i class="fa-solid fa-dragon"></i></button>
+    <button><i class="fa-solid fa-fort"></i></button>
+    <button><i class="fa-regular fa-helmet-battle"></i></button>
+    <button><i class="fa-solid fa-hand-holding-magic"></i></button>
+    <button><i class="fa-regular fa-treasure-chest"></i></button>
+    <button><i class="fa-regular fa-swords"></i></button>
+    <button><i class="fa-regular fa-dungeon"></i></button>
   </nav>
 </header>
 
@@ -36,5 +50,8 @@ nav {
 }
 nav button {
   @apply text-xl py-2 px-5 border border-teal-900 bg-teal-700 text-teal-100 rounded-lg;
+}
+nav button:hover, nav button:focus {
+  @apply bg-teal-600;
 }
 </style>
